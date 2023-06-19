d819c69d7084449c997890de71093ac400e0c0d1
Maryam <maryamjaved.xo@gmail.com>
2023-06-19 19:26:20 +0500

    status codes

--- sandbox.js
+++ sandbox.js
@@ -2,8 +2,12 @@ const request=new XMLHttpRequest();
 
 request.addEventListener('readystatechange', ()=>{
     // console.log(request, request.readyState);
-    if(request.readyState===4){    //state 4 means when it is done 
+    if(request.readyState===4 && request.status===200){    //state 4 means when it is done 
         console.log(request.responseText);
+    }
+
+    else if(request.readyState===4){
+        console.log("could not fetch the data");
     }
 });
 //  args:   type of request,           end point                                   
