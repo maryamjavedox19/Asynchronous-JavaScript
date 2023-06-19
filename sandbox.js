8b33e58c09927a93c54dd8115911b6c68c0b4384
Maryam <maryamjaved.xo@gmail.com>
2023-06-20 00:36:48 +0500

    async and await

--- sandbox.js
+++ sandbox.js
@@ -43,13 +43,33 @@
 
 
 // ------------fetch api---------------------------------
-fetch('todos/luigi.json').then((response) => {
-  console.log('resolved', response);
-  return response.json(); //to get response data
-}).then(data => {
-  console.log(data);
-}).catch((err) => {
-  console.log('rejected', err);
-});
+// fetch('todos/luigi.json').then((response) => {
+//   console.log('resolved', response);
+//   return response.json(); //to get response data
+// }).then(data => {
+//   console.log(data);
+// }).catch((err) => {
+//   console.log('rejected', err);
+// });
+
+
+
+//  Async & await 
+const getTodos = async () => {
+    const response =  await fetch('todos/luigi.json');   //await stops js to assign value to response till promise has resolved
+  
+    if(response.status !== 200){
+      throw new Error('cannot fetch the data');
+    }
+  
+    const data =  await response.json();
+  
+    return data;
+  
+  };
+  
+  getTodos()
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));
 
 
