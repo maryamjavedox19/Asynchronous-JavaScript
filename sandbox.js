a81a9bedd9f6cad36b02d367b06d5d09c0d437ed
Maryam <maryamjaved.xo@gmail.com>
2023-06-19 23:55:20 +0500

    promise

--- sandbox.js
+++ sandbox.js
@@ -1,26 +1,24 @@
-// const getTodos = (resource, callback) => {
-//   const request = new XMLHttpRequest();
-
-//   request.addEventListener("readystatechange", () => {
-//     if (request.readyState === 4 && request.status === 200) {
-//       const data = JSON.parse(request.responseText);
-//       callback(undefined, data);
-//     } else if (request.readyState === 4) {
-//       callback("Could not fetch data", undefined);
-//     }
-//   });
-//     //  args:   type of request,           end point                                   
-//     request.open('GET', 'resource');   //  set up a request     
-//     request.send();
-// }
-
-// getTodos('todos/luigi.json', (err, data) => {
-//     console.log(data);
-//     getTodos('todos/mario.json', (err, data) => {
-//       console.log(data);
-//       getTodos('todos/shaun.json', (err, data) => {
-//         console.log(data);
-//       });
-//     });
-//   });
+const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+      request.addEventListener("readystatechange", () => {
+        if (request.readyState === 4 && request.status === 200) {
+          const data = JSON.parse(request.responseText);
+          resolve(data);
+        } else if (request.readyState === 4) {
+          reject("error getting resource");
+        }
+      });
   
+      request.open('GET', resource); // Use the 'resource' parameter passed to the function
+      request.send();
+    });
+  };
+  
+  getTodos('todos/luigi.json')
+    .then(data => {
+      console.log('promise resolved:', data);
+    })
+    .catch(err => {
+      console.log("promise rejected:", err);
+    });
