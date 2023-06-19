1462b7378d9cf59690aa76203e06cca43536e1b3
Maryam <maryamjaved.xo@gmail.com>
2023-06-19 23:44:35 +0500

    callback hell

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,26 @@
-const getTodos=(callback)=>{
-    const request=new XMLHttpRequest();
+// const getTodos = (resource, callback) => {
+//   const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', ()=>{
-        // console.log(request, request.readyState);
-        if(request.readyState===4 && request.status===200){    //state 4 means when it is done 
-            const data=JSON.parse(request.responseText);  //pasing json string into js object
-            callback(undefined, data);
-        }
-    
-        else if(request.readyState===4){
-            callback("could not fetch data", undefined);
-        }
-    });
-    //  args:   type of request,           end point                                   
-    request.open('GET', 'todos.json');   //  set up a request     
-    request.send();
-}
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState === 4 && request.status === 200) {
+//       const data = JSON.parse(request.responseText);
+//       callback(undefined, data);
+//     } else if (request.readyState === 4) {
+//       callback("Could not fetch data", undefined);
+//     }
+//   });
+//     //  args:   type of request,           end point                                   
+//     request.open('GET', 'resource');   //  set up a request     
+//     request.send();
+// }
 
-getTodos((err, data)=>{
-    console.log("callback fired");
-    if(err){
-        console.log(err);
-    }
-
-    else{
-        console.log(data);
-    }
-
-});
+// getTodos('todos/luigi.json', (err, data) => {
+//     console.log(data);
+//     getTodos('todos/mario.json', (err, data) => {
+//       console.log(data);
+//       getTodos('todos/shaun.json', (err, data) => {
+//         console.log(data);
+//       });
+//     });
+//   });
+  
